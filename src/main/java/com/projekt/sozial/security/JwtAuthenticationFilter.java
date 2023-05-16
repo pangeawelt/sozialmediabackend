8aa6b2953d0be069ecf44f4a5068b19eb86e7a91
Gani Buer <pangeawelt@gmx.de>
2023-05-16 18:38:05 +0200

    configweiter

--- src/main/java/com/projekt/sozial/security/JwtAuthenticationFilter.java
+++ src/main/java/com/projekt/sozial/security/JwtAuthenticationFilter.java
@@ -1,4 +1,44 @@
 package com.projekt.sozial.security;
 
-public class JwtAuthenticationFilter {
-}
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter{
+
+    @Autowired
+    JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    UserDetailsServiceImpl userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        try {
+            String jwtToken = extractJwtFromRequest(request);
+            if(StringUtils.hasText(jwtToken) && jwtTokenProvider.validateToken(jwtToken)) {
+                Long id = jwtTokenProvider.getUserIdFromJwt(jwtToken);
+                UserDetails user = userDetailsService.loadUserById(id);
+                if(user != null) {
+                    UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                    auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(auth);
+                }
+            }
+        } catch(Exception e) {
+            return;
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String extractJwtFromRequest(HttpServletRequest request) {
+        String bearer = request.getHeader("Authorization");
+        if(StringUtils.hasText(bearer) && bearer.startsWith("Bearer "))
+            return bearer.substring("Bearer".length() + 1);
+        return null;
+    }
